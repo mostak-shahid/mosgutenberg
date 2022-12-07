99b8714eb2dc74bf7f3be977eac71538e01c218d
Md. Mostak Shahid <mostak.shahid@gmail.com>
2022-12-07 10:51:17 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -120,16 +120,17 @@ if($btt_enable) :
     <?php if (carbon_get_theme_option( 'mos_plugin_owlcarousel' ) == 'on') : ?>
     <script>
         jQuery(document).ready(function($) {
-            var oc = $('.owl-carousel');
-            var ocOptions = oc.data('carousel-options');
-            var defaults = {
-                loop: true,
-                nav: false,
-                autoplay: true,
-            }
-            oc.owlCarousel($.extend(defaults, ocOptions));
+            $('body').find('.mos-owl-carousel').each(function( e ) {            
+                var oc = $(this);
+                var ocOptions = oc.data('carousel-options');
+                var defaults = {
+                    loop: true,
+                    nav: false,
+                    autoplay: true,
+                }
+                oc.owlCarousel($.extend(defaults, ocOptions));
+            });
         });
-
     </script>
     <?php endif?>
 
