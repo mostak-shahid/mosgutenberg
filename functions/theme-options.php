ef034d33f9797989b258d75cdb7e4e5af526eeed
Md. Mostak Shahid <mostak.shahid@gmail.com>
2022-12-14 18:58:31 +0600

    Update theme-options.php

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -8,42 +8,42 @@ function mos_theme_options() {
     ->set_icon('dashicons-admin-customizer')
     ->add_fields(array(
         Field::make('image', 'mos-logo', __('Logo'))
-        ->set_default_value( get_template_directory_uri() . '/assets/img/logo.svg' ),
+        ->set_default_value(get_template_directory_uri() . '/assets/img/logo.svg'),
         Field::make('header_scripts', 'crb_header_script', __('Header Script')),
         Field::make('footer_scripts', 'crb_footer_script', __('Footer Script')),
-    ));
+   ));
 
     Container::make('theme_options', __('Color scheme'))
     ->set_page_parent($basic_options_container) // reference to a top level container
     ->add_fields(array(
-        Field::make( 'color', 'mos_body_bg', 'Body Background' )
-        //->set_palette( array( '#FF0000', '#00FF00', '#0000FF' ))
-        ->set_alpha_enabled( true )
-        ->set_help_text( 'Pick the color for body background, by default it is set to #ffffff. You can use this color in your css with var(--mos-body-bg)' )
-        ->set_default_value( '#ffffff' ),
+        Field::make('color', 'mos_body_bg', 'Body Background')
+        //->set_palette(array('#FF0000', '#00FF00', '#0000FF'))
+        ->set_alpha_enabled(true)
+        ->set_help_text('Pick the color for body background, by default it is set to #ffffff. You can use this color in your css with var(--mos-body-bg)')
+        ->set_default_value('#ffffff'),
         
-        Field::make( 'color', 'mos_primary_color', 'Primary Color' )
-        ->set_help_text( 'Pick the primary color, by default it is set to #00f5eb. You can use this color in your css with var(--mos-primary-color)' )
-        ->set_default_value( '#00f5eb' ),
+        Field::make('color', 'mos_primary_color', 'Primary Color')
+        ->set_help_text('Pick the primary color, by default it is set to #00f5eb. You can use this color in your css with var(--mos-primary-color)')
+        ->set_default_value('#00f5eb'),
         
-        Field::make( 'color', 'mos_secondary_color', 'Secondary Color' )
-        ->set_help_text( 'Pick the secondary color, by default it is set to #21fff6. You can use this color in your css with var(--mos-secondary-color)' )
-        ->set_default_value( '#21fff6' ),
+        Field::make('color', 'mos_secondary_color', 'Secondary Color')
+        ->set_help_text('Pick the secondary color, by default it is set to #21fff6. You can use this color in your css with var(--mos-secondary-color)')
+        ->set_default_value('#21fff6'),
         
-        Field::make( 'color', 'mos_content_color', 'Content Color' )
-        ->set_help_text( 'Pick the content color, by default it is set to #212529. You can use this color in your css with var(--mos-content-color)' )
-        ->set_default_value( '#212529' ),
-        Field::make( 'color', 'mos_link_color', 'Link Color' )
-        ->set_help_text( 'Pick the link color, by default it is set to #015ea5.' )
-        ->set_default_value( '#015ea5' ),
-        Field::make( 'color', 'mos_link_hover_color', 'Link Hover Color' )
-        ->set_help_text( 'Pick the link hover color, by default it is set to #0a58ca.' )
-        ->set_default_value( '#0a58ca' ),
-    ));
+        Field::make('color', 'mos_content_color', 'Content Color')
+        ->set_help_text('Pick the content color, by default it is set to #212529. You can use this color in your css with var(--mos-content-color)')
+        ->set_default_value('#212529'),
+        Field::make('color', 'mos_link_color', 'Link Color')
+        ->set_help_text('Pick the link color, by default it is set to #015ea5.')
+        ->set_default_value('#015ea5'),
+        Field::make('color', 'mos_link_hover_color', 'Link Hover Color')
+        ->set_help_text('Pick the link hover color, by default it is set to #0a58ca.')
+        ->set_default_value('#0a58ca'),
+   ));
     
     Container::make('theme_options', __('Theme Resourcess'))
     ->set_page_parent($basic_options_container) // reference to a top level container
-    ->add_fields(array(                    
+    ->add_fields(array(                   
 
         Field::make('radio', 'mos_plugin_bootstrap', __('Bootsrap'))
         ->set_options(array(
@@ -54,7 +54,7 @@ function mos_theme_options() {
             'bootstrap-bundle' => 'Bundle CSS',
             'seperated-files' => 'Seperated Files',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value('bootstrap-bundle'),
         
         Field::make('multiselect', 'crb_select', __('Files'))
@@ -62,115 +62,115 @@ function mos_theme_options() {
             'bootstrap-reboot' => 'Reboot CSS',
             'bootstrap-grid' => 'Grid CSS',
             'bootstrap-utilities' => 'Utilities CSS',
-        ))
+       ))
         ->set_conditional_logic(array(
             'relation' => 'AND', // Optional, defaults to "AND"
             array(
                 'field' => 'mos_plugin_bootstrap',
                 'value' => 'seperated-files', // Optional, defaults to "". Should be an array if "IN" or "NOT IN" operators are used.
                 'compare' => '=', // Optional, defaults to "=". Available operators: =, <, >, <=, >=, IN, NOT IN
-          )
-        )),
+         )
+       )),
 
         Field::make('radio', 'mos_plugin_jquery', __('Jquery'))
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value('on'),
 
         Field::make('radio', 'mos_plugin_fontawesome', __('Font Awesome'))
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value('on'),         
 
         Field::make('radio', 'mos_plugin_fancybox', __('Fancybox'))
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value(['off']),
 
         Field::make('radio', 'mos_plugin_isotop', __('Isotop'))
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value('off'),
 
         Field::make('radio', 'mos_plugin_card_slider', __('Card Slider'))
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value(['off']),
         Field::make('radio', 'mos_plugin_jpages', __('jPages'))
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value(['off']),
         Field::make('radio', 'mos_plugin_lazyload', __('Lazy Load'))
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value(['off']),
         Field::make('radio', 'mos_plugin_table_shrinker', __('Table Shrinker'))
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value(['off']),
         Field::make('radio', 'mos_plugin_owlcarousel', __('Owl Carousel'))
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value(['off']),
         Field::make('radio', 'mos_plugin_slick', __('Slick Slider'))
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value(['off']),
         Field::make('radio', 'mos_plugin_wow', __('Wow'))
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value(['off']),
         Field::make('radio', 'mos_plugin_animate', __('Animate CSS'))
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value(['off']),
         Field::make('radio', 'mos_plugin_jflip', __('Jquery Flip'))
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value(['off']),  
-        Field::make( 'complex', 'mos_plugin_additional', __( 'Additional Assets' ) )
-        ->add_fields( array(
-            Field::make( 'select', 'type', __( 'Asset Type' ))
-            ->set_options( array(
+        Field::make('complex', 'mos_plugin_additional', __('Additional Assets'))
+        ->add_fields(array(
+            Field::make('select', 'type', __('Asset Type'))
+            ->set_options(array(
                 'style' => 'CSS',
                 'script' => 'Script',
-            )),
-            Field::make( 'select', 'from', __( 'From' ))
-            ->set_options( array(
+           )),
+            Field::make('select', 'from', __('From'))
+            ->set_options(array(
                 'parent' => 'Parent Theme',
                 'child' => 'Child Theme',
                 'outside' => 'CDN/Outside',
-            )),
-            Field::make( 'text', 'source', __( 'Source' )),
-        )),
+           )),
+            Field::make('text', 'source', __('Source')),
+       )),
     
-    ));
+   ));
     Container::make('theme_options', __('Contact Info'))
     ->set_page_parent($basic_options_container) // reference to a top level container
     ->add_fields(array(
@@ -178,31 +178,31 @@ function mos_theme_options() {
         ->add_fields(array(
             Field::make('text', 'title', __('Title')),
             Field::make('text', 'number', __('Phone Number')),
-        )),
+       )),
         Field::make('complex', 'mos-contact-email', __('Email'))
         ->add_fields(array(
             Field::make('text', 'title', __('Title')),
             Field::make('text', 'email', __('Email Address')),
-        )),
+       )),
         Field::make('complex', 'mos-contact-business-hours', __('Business Hours'))
         ->add_fields(array(
             Field::make('text', 'title', __('Title')),
             Field::make('text', 'hours', __('Business Hours')),
-        )),
+       )),
         Field::make('complex', 'mos-contact-contact-address', __('Contact Address'))
         ->add_fields(array(
             Field::make('text', 'title', __('Title')),
             Field::make('text', 'address', __('Address')),
             Field::make('text', 'link', __('Map Link')),
-        )),
+       )),
         Field::make('complex', 'mos-contact-social-media', __('Social Media'))
         ->add_fields(array(
             Field::make('text', 'title', __('Title')),
             Field::make('text', 'link', __('Link')),
             Field::make('checkbox', 'new-tab', __('Open in new tab'))
             ->set_option_value('no'),
-        )),
-    ));
+       )),
+   ));
     Container::make('theme_options', __('Back to Top'))
     ->set_page_parent($basic_options_container) // reference to a top level container
     ->add_fields(array(
@@ -210,11 +210,11 @@ function mos_theme_options() {
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value(['on']),
         Field::make('image', 'mos-back-to-top-image', __('Back to top image')),
         Field::make('text', 'mos-back-to-top-class', __('Back to top class')),
-    ));
+   ));
     Container::make('theme_options', __('Page Loader'))
     ->set_page_parent($basic_options_container) // reference to a top level container
     ->add_fields(array(
@@ -222,13 +222,13 @@ function mos_theme_options() {
         ->set_options(array(
             'on' => 'Enabled',
             'off' => 'Disabled',
-        ))
+       ))
         ->set_default_value(['on']),
         Field::make('image', 'mos-page-loader-image', __('Page loader image')),
         Field::make('color', 'mos-page-loader-background', 'Page loader background')
         ->set_alpha_enabled(true),
         Field::make('text', 'mos-page-loader-class', __('Page loader class')),
-    ));
+   ));
 
     Container::make('theme_options', __('Header Section'))
     ->set_page_parent($basic_options_container) // reference to a top level container
@@ -257,7 +257,7 @@ function mos_theme_options() {
                 'bottom left' => 'Bottom left',
                 'bottom center' => 'Bottom Center',
                 'bottom right' => 'Bottom Right',
-            ))
+           ))
             ->set_default_value(['top left']),
             Field::make('select', 'background-size', __('Background Size'))
             ->set_options(array(
@@ -269,7 +269,7 @@ function mos_theme_options() {
                 'revert' => 'revert',
                 'revert-layer' => 'revert-layer',
                 'unset' => 'unset',
-            ))
+           ))
             ->set_default_value(['cover']),
             //background-repeat: repeat|repeat-x|repeat-y|no-repeat|initial|inherit;
             Field::make('select', 'background-repeat', __('Background Repeat'))
@@ -280,16 +280,16 @@ function mos_theme_options() {
                 'no-repeat' => 'no-repeat',
                 'initial' => 'initial',
                 'inherit' => 'inherit',
-            ))
+           ))
             ->set_default_value(['scroll']),
             Field::make('select', 'background-attachment', __('Background Attachment'))
             ->set_options(array(
                 'scroll' => 'Scroll',
                 'fixed' => 'Fixed',
-            ))
+           ))
             ->set_default_value(['scroll']),
-        )),
-    ));
+       )),
+   ));
     Container::make('theme_options', __('Footer Section'))
     ->set_page_parent($basic_options_container) // reference to a top level container
     ->add_fields(array(
@@ -318,7 +318,7 @@ function mos_theme_options() {
                 'bottom left' => 'Bottom left',
                 'bottom center' => 'Bottom Center',
                 'bottom right' => 'Bottom Right',
-            ))
+           ))
             ->set_default_value(['top left']),
             Field::make('select', 'background-size', __('Background Size'))
             ->set_options(array(
@@ -330,7 +330,7 @@ function mos_theme_options() {
                 'revert' => 'revert',
                 'revert-layer' => 'revert-layer',
                 'unset' => 'unset',
-            ))
+           ))
             ->set_default_value(['cover']),
             //background-repeat: repeat|repeat-x|repeat-y|no-repeat|initial|inherit;
             Field::make('select', 'background-repeat', __('Background Repeat'))
@@ -341,14 +341,14 @@ function mos_theme_options() {
                 'no-repeat' => 'no-repeat',
                 'initial' => 'initial',
                 'inherit' => 'inherit',
-            ))
+           ))
             ->set_default_value(['scroll']),
             Field::make('select', 'background-attachment', __('Background Attachment'))
             ->set_options(array(
                 'scroll' => 'Scroll',
                 'fixed' => 'Fixed',
-            ))
+           ))
             ->set_default_value(['scroll']),
-        )),
-    ));
+       )),
+   ));
 }
